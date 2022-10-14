7355203e4782c5233a643bb142bf46e50d20f4d8
shetkart <shetkarthik89@gmail.com>
2022-10-14 16:31:26 +0530

    commit

---
+++ src/Components/home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../App.css"
+
+
+export const Home = ()=>{
+    return(
+  <>
+    <h1>Hello Home,</h1>
+   
+    <div className="para">
+    <h2>Cafe</h2>
+      <p className="text">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus fugiat autem voluptatem. Ea iure adipisci perferendis, laboriosam dicta asperiores placeat, maiores fuga error illo deserunt possimus quasi! Qui repudiandae temporibus praesentium nulla, numquam nisi natus nesciunt similique, id quaerat eligendi, odio autem corrupti alias beatae perspiciatis molestias. Deserunt quam cum temporibus similique, quod eos hic. Perferendis, magnam. Nihil libero beatae, distinctio culpa nobis ea sed expedita officiis a totam ratione reiciendis, soluta numquam."</p>
+      <button type="button" class="btn btn-dark">Join</button>
+    </div>
+  </>
+    );
+}
